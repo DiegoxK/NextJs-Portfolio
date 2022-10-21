75a3e39826a42e13f7b2c258ae8bd69a2d84c03f
DiegoxK <diego.stv@outlook.es>
2022-10-21 13:16:07 -0500

    Social media anchors

--- components/SocialMedia.jsx
+++ components/SocialMedia.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import React from "react";
+import { BsTwitter, BsInstagram } from "react-icons/bs";
+// import { FaFacebookF } from "react-icons/fa";
+import { AiFillGithub } from "react-icons/ai";
 
 const SocialMedia = () => (
   <div className="app__social">
-    <div>
+    <a href="https://twitter.com/DiegoxKa">
       <BsTwitter />
-    </div>
-    <div>
-      <FaFacebookF />
-    </div>
-    <div>
+    </a>
+    <a href="https://github.com/DiegoxK">
+      <AiFillGithub />
+    </a>
+    <a href="https://www.instagram.com/diegoxka/">
       <BsInstagram />
-    </div>
+    </a>
   </div>
 );
 
